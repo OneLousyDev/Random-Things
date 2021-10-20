c5623339cf130be579185524dbf3c7fdfd4a9efb
OneLousyDev <lousy@lousiesmods.com>
2021-10-20 05:57:38 +0100

    And so begins the port to 1.16

---
+++ src/main/java/com/lousiesmods/randomthings/RandomThings.java
@@ -0,0 +1,56 @@
+package com.lousiesmods.randomthings;
+
+import com.lousiesmods.randomthings.util.Reference;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Reference.MOD_ID)
+public class RandomThings
+{
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public static final ResourceLocation LOCATION(String id)
+    {
+        return new ResourceLocation(Reference.MOD_ID, id);
+    }
+
+    public RandomThings()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event)
+    {
+
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+
+    }
+
+    private void processIMC(final InterModProcessEvent event)
+    {
+
+    }
+}
---
+++ src/main/java/com/lousiesmods/randomthings/util/Reference.java
@@ -0,0 +1,6 @@
+package com.lousiesmods.randomthings.util;
+
+public class Reference
+{
+    public static final String MOD_ID = "randomthings";
+}
